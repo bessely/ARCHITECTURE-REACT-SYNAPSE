ab87778c09bd09d517091596082b00c0ac333c8e
bessely <besselymail@gmail.com>
2024-03-04 14:54:48 +0000

    pagination revue

--- src/globalComponents/Pagination.jsx
+++ src/globalComponents/Pagination.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactPaginate from "react-paginate";
-import { getCurrentPath } from '../services/globalFunction';
 export const PAGINATION = {listParPage:10, currentPage:0, changePageClick:false, listLenght:0, nbrPage:0};
-export function Pagination({onClick,nbrPage, forcePage}) {
-  if (getCurrentPath() === "Rapprochement" || getCurrentPath() === "Validation" || getCurrentPath() === "Derapprochement" || getCurrentPath() === "Societe" || getCurrentPath() === "Valeur") {
+/**
+ * 
+ * @param {function} onClick  la function callback a executé qd on cliquera sur un button de pagination
+ * @param {integer} nbrPage  le nombre de page maximum a afficher
+ * @param {boolean} nbrPage  activer le mode forcerPage ou pas 
+ * @param {string} typeStyle  le style de pagination a renvoyer [classic,compact], classique par defaut
+ * @returns 
+ */
+export function Pagination({onClick,nbrPage, forcePage,typeStyle="classic"}) {
+  if (typeStyle==="compact") {
         return(
           <div className="m-0 paginating-container pagination-default d-flex justify-content-end">
             <ReactPaginate
